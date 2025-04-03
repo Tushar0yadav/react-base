b9679165f7be8cd7d6390a9f33d8e556962d4843
Tushar0yadav <126134177+Tushar0yadav@users.noreply.github.com>
2025-04-03 08:32:19 +0530

    enhance image project

---
+++ 01first-react/Image Enhancer/src/components/Home.jsx
@@ -0,0 +1,32 @@
+import { enhancedImageAPI } from "../utils/enhancedImageAPI";
+import ImagePreview from "./ImagePreview";
+import ImageUpload from "./ImageUpload";
+import { useState } from "react";
+
+const Home = () => {
+  const [uploadImage, setUploadImage] = useState(null);
+  const [enhancedImage, setEnhancedImage] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const UploadImageHandler = async (file) => {
+    setUploadImage(URL.createObjectURL(file));
+    setLoading(true);
+    try { const enhancedURL = await enhancedImageAPI(file);
+      setEnhancedImage(enhancedURL);
+      setloading(false);   
+    } catch (error) {console.log(error);
+      alert("Error while enhancing the image. Please try again later."); }
+  };
+  return (
+    <>
+      <ImageUpload UploadImageHandler={UploadImageHandler} />
+      <ImagePreview
+        loading={loading}
+        uploaded={uploadImage}
+        enhanced={enhancedImage?.image}
+      />
+    </>
+  );
+};
+
+export default Home;
